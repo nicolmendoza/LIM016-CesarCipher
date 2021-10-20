8e25d21d20b8c5a44c804720ff4ba35bef3764db
nicolmendoza <87873460+nicolmendoza@users.noreply.github.com>
2021-10-19 21:12:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,43 @@
 import cipher from './cipher.js';
 
 
-let resultado = document.getElementById("resultado")
-let resultadoDes = document.getElementById('resultadoDescifrado')
-var mensajero = document.getElementById('mensaje')
+
+var mensaje = document.getElementById('mensaje')
 var dezplazamiento = document.getElementById('desplazamiento')
-const enviar=document.getElementById('enviar')
+const cifrar=document.getElementById('cifrar')
 const descifrar=document.getElementById('descifrar')
 const copiar=document.getElementById('copiar')
 const limpiar=document.getElementById('limpiar')
 
-let mnj;
-let num;
 
 
+cifrar.addEventListener('click',(e)=> {
 
-enviar.addEventListener('click',()=> {
+e.preventDefault()
+    let ecp =  cipher.encode(Number(dezplazamiento.value),mensaje.value)
+    mensaje.value = ecp
 
-     mnj = mensajero.value;
-     num = Number(dezplazamiento.value);
-    // console.log(mnj, num);
-    cipher.encode(num,mnj )
-
-    // console.log(cipher.encode(mnj, num));
-    var ecp = resultado.textContent = cipher.encode(num,mnj)
-    return mensajero.value = ecp
-
+    return mensaje
 })
 
 
+descifrar.addEventListener('click',(e)=>{
 
+e.preventDefault()
+    var dcp = cipher.decode( Number(dezplazamiento.value),mensaje.value)
+    mensaje.value = dcp
 
-descifrar.addEventListener('click',()=>{
-
-
-    cipher.decode( Number(dezplazamiento.value),mensajero.value)
-
-    // console.log(cipher.decode(mensajero.value, Number(dezplazamiento.value)))
-    var ecp = resultadoDes.textContent = cipher.decode( Number(dezplazamiento.value),mensajero.value)
-    return mensajero.value = ecp
+    return mensaje
 })
-
 
 
 copiar.addEventListener('click',(e)=>{
   e.preventDefault()
-
   document.getElementById('mensaje').select()
-let texto=mensajero.value
-console.log(typeof(texto));
-
-
 document.execCommand('copy')
 })
 
 limpiar.addEventListener('click',(e)=>{
   e.preventDefault()
-  mensajero.value=""
+  mensaje.value=""
 })
