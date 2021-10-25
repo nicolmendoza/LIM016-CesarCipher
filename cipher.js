d63565bd416aa018a40cab52be8ba886f485fa1f
nicolmendoza <87873460+nicolmendoza@users.noreply.github.com>
2021-10-25 09:43:32 -0500

    últimos cambios

--- cipher.js
+++ cipher.js
@@ -1,7 +1,6 @@
 const cipher = {
 
   encode: (esp, txt) => {
-
     if (esp == null || txt == []) {
       throw new TypeError
 
@@ -14,37 +13,40 @@ const cipher = {
         var valor = txt.charCodeAt(i)
 
         if (65 <= valor && valor <= 90) {
-          let comprobar = (valor - 65 + esp) % 26
 
-          if (comprobar < 0) {
+          let comprobarM = (valor - 65 + esp) % 26
 
-            nuevoV = (valor - 65 + esp) % 26 + 91
+          if (comprobarM < 0) {
+
+            nuevoV = comprobarM + 91
           }
           else {
-            nuevoV = (valor - 65 + esp) % 26 + 65
+            nuevoV = comprobarM + 65
           }
 
         }
         else if (97 <= valor && valor <= 122) {
 
-          let comprobar = (valor - 97 + esp) % 26
+          let comprobarMN = (valor - 97 + esp) % 26
 
-          if (comprobar < 0) {
+          if (comprobarMN < 0) {
 
-            nuevoV = (valor - 97 + esp) % 26 + 123
+            nuevoV = comprobarMN + 123
           }
           else {
-            nuevoV = (valor - 97 + esp) % 26 + 97
+            nuevoV = comprobarMN + 97
           }
         }
 
         else if (48 <= valor && valor <= 57) {
-          let comprobar = (valor - 48 + esp) % 10
-          if (comprobar < 0) {
-            nuevoV = (valor - 48 + esp) % 10 + 58
+
+          let comprobarNM = (valor - 48 + esp) % 10
+
+          if (comprobarNM < 0) {
+            nuevoV = comprobarNM + 58
           }
           else {
-            nuevoV = (valor - 48 + esp) % 10 + 48
+            nuevoV = comprobarNM + 48
           }
         }
 
@@ -63,67 +65,62 @@ const cipher = {
 
 
 
-  decode: (esp, ecp) => {
+  decode: (esp, txt) => {
 
-    if (esp == null || ecp == []) {
+    if (esp == null || txt == []) {
       throw new TypeError
 
     }
     else {
 
-    let resultado2 = "";
-    let nuevoV;
-    for (let i = 0; i < ecp.length; i++) {
-      var valor = ecp.charCodeAt(i)
+      let resultado2 = "";
+      let nuevoV;
+      for (let i = 0; i < txt.length; i++) {
+        var valor = txt.charCodeAt(i)
 
-      if (65 <= valor && valor <= 90) {
-        let comprobar = (valor - 65 - esp) % 26
-        if (comprobar < 0) {
+        if (65 <= valor && valor <= 90) {
+          let comprobarMa = (valor - 65 - esp) % 26
+          if (comprobarMa < 0) {
 
-          nuevoV = (valor - 65 - esp) % 26 + 91
+            nuevoV = comprobarMa + 91
+          }
+          else {
+            nuevoV = comprobarMa + 65
+          }
+
+
         }
+        else if (97 <= valor && valor <= 122) {
+          let comprobarMn = (valor - 97 - esp) % 26
+          if (comprobarMn < 0) {
+            nuevoV = comprobarMn + 123
+          }
+          else {
+            nuevoV = comprobarMn + 97
+          }
+
+        }
+        else if (48 <= valor && valor <= 57) {
+          let comprobarNum = (valor - 48 - esp) % 10
+          if (comprobarNum < 0) {
+            nuevoV = comprobarNum + 58
+          }
+          else {
+            nuevoV = comprobarNum + 48
+          }
+        }
+
+
         else {
-          nuevoV = (valor - 65 - esp) % 26 + 65
+          nuevoV = valor
         }
-
+        resultado2 += String.fromCharCode(nuevoV)
 
       }
-      else if (97 <= valor && valor <= 122) {
-        let comprobar = (valor - 97 - esp) % 26
-        if (comprobar < 0) {
-          nuevoV = (valor - 97 - esp) % 26 + 123
-        }
-        else {
-          nuevoV = (valor - 97 - esp) % 26 + 97
-        }
 
-      }
-      else if (48 <= valor && valor <= 57) {
-        let comprobar = (valor - 48 - esp) % 10
-        if (comprobar < 0) {
-          nuevoV = (valor - 48 - esp) % 10 + 58
-        }
-        else {
-          nuevoV = (valor - 48 - esp) % 10 + 48
-        }
-      }
-
-
-      else {
-        nuevoV = valor
-      }
-      resultado2 += String.fromCharCode(nuevoV)
-
+      return resultado2
     }
-
-    return resultado2
   }
-
-
-
-
-
-}
 }
 
 export default cipher;
